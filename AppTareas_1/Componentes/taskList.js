347c0686f4e2aa94e962c90eab5cf53cebb7a0b9
Joyssi <joyssicruz5@gmail.com>
2024-09-30 19:08:43 -0600

    Edición del archivo App.js

--- AppTareas_1/Componentes/taskList.js
+++ AppTareas_1/Componentes/taskList.js
@@ -1,21 +1,48 @@
-import React from 'react';
-import { FlatList } from 'react-native';
-import TareaItem from './taskItem';
+    import React, { useState } from 'react';
+    import { View, Text, StyleSheet } from 'react-native';
+    import TareaInput from './Componentes/taskInput';  
+    import TareaLista from './Componentes/taskList';  
 
-const TareaLista = ({ tareas, eliminarTarea, toggleCompletada }) => {
-    return (
-        <FlatList
-        data={tareas}
-        renderItem={({ item }) => (
-            <TareaItem
-            tarea={item}
-            eliminarTarea={eliminarTarea}
-            toggleCompletada={toggleCompletada}  // Pasar la función para alternar
-            />
-        )}
-        keyExtractor={(item) => item.id}
-        />
-    );
+    export default function App() {
+    const [tareas, setTareas] = useState([]);
+
+    const agregarTarea = (texto) => {
+        setTareas([...tareas, { id: Date.now().toString(), texto, completada: false }]);
     };
 
-export default TareaLista;
+    const eliminarTarea = (id) => {
+        setTareas(tareas.filter((tarea) => tarea.id !== id));
+    };
+
+    const toggleCompletada = (id) => {
+        setTareas(
+        tareas.map((tarea) =>
+            tarea.id === id ? { ...tarea, completada: !tarea.completada } : tarea
+        )
+        );
+    };
+
+    return (
+        <View style={styles.contenedorPrincipal}>
+        <Text style={styles.titulo}>Aplicación de Tareas</Text>
+        <TareaInput agregarTarea={agregarTarea} />
+        <TareaLista tareas={tareas} eliminarTarea={eliminarTarea} toggleCompletada={toggleCompletada} />
+        </View>
+    );
+    }
+
+    const styles = StyleSheet.create({
+    contenedorPrincipal: {
+        paddingTop: 50,
+        paddingHorizontal: 20,
+        flex: 1,
+        backgroundColor: '#f2f2f2',
+    },
+    titulo: {
+        textAlign: 'center',
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#1630BE',
+        marginBottom: 15,
+    },
+});
